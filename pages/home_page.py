e821b6e5c0143e05529174ed364d4d50b3a4e563
Atul Tiwari <atultiwari129@gmail.com>
2025-07-12 11:02:24 +0530

    Add New Code

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,3 +6,12 @@ class HomePage(BasePage):
 
     def search(self, text):
         self.enter_text(self.SEARCH_BOX, text)
+
+
+
+
+        teddf        self.submit_search()    def submit_search(self):
+        search_button = (By.ID, "search-button")
+
+
+        
